7e475294a9eb196fba519cc57c19f58fc2944067
ankursanghi <ankur.sanghi@gmail.com>
2015-04-26 22:33:01 -0400

    Testing the commit on a branch

--- meadowlark.js
+++ meadowlark.js
@@ -14,6 +14,7 @@ var fortunes = [
 "Conquer your fears or they will conquer you.", "Rivers need springs.",
 "Do not fear what you don't know.",
 "You will have a pleasant surprise.", "Whenever possible, keep it simple.",
+"You will get many friends without making a special effort."
 ];
 app.get('/about', function(req, res){
 		var randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)];
