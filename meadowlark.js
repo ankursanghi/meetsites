fe5deb1938863b62655d76832ca8d743622cff5e
ankursanghi <ankur.sanghi@gmail.com>
2015-04-26 23:20:26 -0400

    Added fortune to a module

--- meadowlark.js
+++ meadowlark.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fortune = require('./lib/fortune.js');
 var app = express();
 app.set('port', process.env.PORT || 8888);
 
@@ -10,15 +11,8 @@ app.set('view engine', 'handlebars');
 app.get('/', function(req, res){ 
 		res.render('home');
 		});
-var fortunes = [
-"Conquer your fears or they will conquer you.", "Rivers need springs.",
-"Do not fear what you don't know.",
-"You will have a pleasant surprise.", "Whenever possible, keep it simple.",
-"You will get many friends without making a special effort."
-];
 app.get('/about', function(req, res){
-		var randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)];
-		res.render('about', {fortune: randomFortune});
+		res.render('about', {fortune: fortune.getFortune()});
 		});
 
 // Declare static content location
