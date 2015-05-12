3eecd12ece2a32e936e9abc00805f492f6d5d6d6
ankursanghi <ankur.sanghi@gmail.com>
2015-05-12 19:11:39 -0400

    Committing after a few days. This save point has the template page working.

--- meadowlark.js
+++ meadowlark.js
@@ -7,14 +7,28 @@ app.set('port', process.env.PORT || 8888);
 var handlebars = require('express-handlebars') .create({ defaultLayout:'main' });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
+//
+// middleware to detect test=1 in querystring
+app.use(function(req, res, next){
+		res.locals.showTests = app.get('env') !== 'production' &&
+		req.query.test === '1';
+		next();
+		});
 
 app.get('/', function(req, res){ 
 		res.render('home');
 		});
 app.get('/about', function(req, res){
-		res.render('about', {fortune: fortune.getFortune()});
+		res.render('about', {fortune: fortune.getFortune(), pageTestScript:'./public/qa/tests-about.js'});
 		});
-
+app.get('/headers', function(req,res){
+		res.set('Content-Type','text/plain');
+		var s='';
+		for(var name in req.headers) s += name + ': ' + req.headers[name] + '\n'; res.send(s);
+		});
+app.get('/csstest', function(req,res){
+		res.render('csstest', { layout: false});
+		});
 // Declare static content location
 app.use(express.static(__dirname + '/public'));
 
